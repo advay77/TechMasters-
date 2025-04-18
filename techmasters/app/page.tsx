15ba56f0ae1dff6634cae0586b1e028e850ff028
advay77 <advayanand37@gmail.com>
2025-04-18 21:17:00 +0530

    Initial commit

--- techmasters/app/page.tsx
+++ techmasters/app/page.tsx
@@ -1,12 +1,14 @@
 import { GallerySection } from "@/components/events/gallery-section";
 import { AboutSection } from "@/components/home/about-section";
 import { Team } from "@/components/home/team";
+import Hero from "@/components/hero/hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <Team/>
-    </div>
-      );
+    <main>
+      <Hero />
+      <Team />
+    </main>
+  );
 }
