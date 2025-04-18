fe8b1671c23ee94cdc07180cd624842cb9bbca9d
Divyansh Goel <divyanshgoel8860@gmail.com>
2025-04-18 13:29:42 +0530

    Added Gallery, team, About Section Comp

--- techmasters/app/page.tsx
+++ techmasters/app/page.tsx
@@ -1,9 +1,12 @@
+import { GallerySection } from "@/components/events/gallery-section";
+import { AboutSection } from "@/components/home/about-section";
+import { Team } from "@/components/home/team";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      
+      <Team/>
     </div>
       );
 }
